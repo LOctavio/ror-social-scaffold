356d77824bb8efcd74cb29d48230ab7a2765a050
LOctavio <luis_ram1997@hotmail.com>
2020-07-14 10:25:14 -0500

    Add pending_friends and friend? method

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,11 +11,19 @@ class User < ApplicationRecord
   has_many :likes, dependent: :destroy
   has_many :friendships
   has_many :friends, class_name: 'Friendship', foreign_key: 'friend_id'
-  has_many :inverse_frienships, :class_name => 'Friendship', :foreign_key = 'friend_id'
+  has_many :inverse_friendships, :class_name => 'Friendship', foreign_key: 'friend_id'
 
   def friends
     friends_array = friendships.map{|friendship| friendship.friend if friendship.status}
     friends_array + inverse_friendships.map{|friendship| friendship.user if friendship.status}
     friends_array.compact
+  end
+
+  def pending_friends
+    friendships.map{|friendship| friendship.friend if !friendship.confirmed}.compact
+  end
+
+  def friend?(user)
+    friends.include?(user)
   end
 end
