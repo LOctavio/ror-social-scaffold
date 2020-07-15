bf1ba6b185d65002c7913b949d12cf252f183ac8
Nurislam Ab <you@example.com>
2020-07-16 02:05:08 +0500

    Change index action

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,19 +1,20 @@
 class FriendshipsController < ApplicationController
 
     def index
-        @friend_list = current_user.friendships.all
+        @friend_list = current_user.friends
     end
     
     def create
         @friend = current_user.friendships.create(friendship_params)
 
+        
         redirect_to root_path
     end
 
     private
 
     def friendship_params
-        params.permit(:user_id, :friend_id, :status)
+        params.permit(:user_id, :friend_id)
     end
 
 end
