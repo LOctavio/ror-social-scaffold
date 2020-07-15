56be03d41c63549905ecadcdb5dd798ede851cd5
Nurislam Ab <you@example.com>
2020-07-16 02:05:52 +0500

    Add friend requests action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,4 +9,8 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @posts = @user.posts.ordered_by_most_recent
   end
+
+  def friend_requests_received
+    @friend_requests_received = current_user.friend_requests
+  end
 end
