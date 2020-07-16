2328bf4702db0548c215ff4eb4b0527b5143f932
Nurislam Ab <you@example.com>
2020-07-16 20:55:10 +0500

    Add friend_requests_received method for the Friendship model

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,4 +1,11 @@
 class Friendship < ApplicationRecord
     belongs_to :user
     belongs_to :friend, class_name: 'User'
+
+    validates_uniqueness_of :user_id, scope: :friend_id
+
+    def self.friend_requests_received(current_user)
+        find_by_sql(["SELECT * FROM users u JOIN friendships f ON u.id = f.user_id
+            WHERE f.status = false AND f.friend_id = ?", current_user])
+    end
 end
