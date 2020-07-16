ff545949f469e378ce4a63ee769f4e9adb771762
Nurislam Ab <you@example.com>
2020-07-16 20:55:56 +0500

    Change user model methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,26 +11,25 @@ class User < ApplicationRecord
   has_many :likes, dependent: :destroy
   has_many :friendships
   has_many :friends, through: :friendships #class_name: 'Friendship', foreign_key: 'friend_id'
-  has_many :inverse_friendships, :class_name => 'Friendship', foreign_key: 'friend_id'
+  has_many :inverse_friendships, class_name: 'Friendship', foreign_key: 'friend_id'
 
   def friends
     friends_array = friendships.map{|friendship| friendship.friend if friendship.status}
-    friends_array + inverse_friendships.map{|friendship| friendship.user if friendship.status}
+    # friends_array + inverse_friendships.map{|friendship| friendship.user if friendship.status}
     friends_array.compact
   end
 
-  def pending_friends
-    friendships.map{|friendship| friendship.friend if !friendship.status}.compact
+  def confirm_friend(user_id)
+    friendship = inverse_friendships.find{|friendship| friendship.user_id == user_id && friendship.status = false}
+    friendship.update_attribute(:status, true)
   end
 
   def friend?(user)
     friends.include?(user)
   end
 
-  def confirm_friend(user)
-    friendship = inverse_friendships.find{|friendship| friendship.user == user}
-    friendship.status = true
-    friendship.save
+  def pending_friends
+    friendships.map{|friendship| friendship.friend if !friendship.status}.compact
   end
 
   # Users who have requested to be friends
