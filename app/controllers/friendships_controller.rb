9fc88602677eab0945e64e279beeeffa440984c4
Nurislam Ab <you@example.com>
2020-07-16 20:51:57 +0500

    Refactor friendship controller actions

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,30 +1,40 @@
 class FriendshipsController < ApplicationController
 
     def index
-        @friend_list = current_user.friends
+      @friend_list = current_user.friends
     end
     
     def create
-        @friend = current_user.friendships.create(friendship_params)
-
-        
-        redirect_to root_path
+      @friend = current_user.friendships.build(friend_id: params[:user_id])
+      
+      if @friend.save
+        redirect_back(fallback_location: root_path, notice: 'Friendship request was sent.')
+      else
+        redirect_back(fallback_location: users_path)
+      end
     end
 
     def update
-        @friend = Friendship.find_by(params[:friend_id])
+      @friendship = Friendship.find(params[:friendship])
 
-        
+        return unless @friendship.update(friendship_params)
+
+        flash[:notice] = 'Friend request accepted.'
+        redirect_to requests_path
     end
 
     def destroy
+      @friendship = Friendship.find(params[:friendship])
+          
+      return unless @friendship.destroy
 
+      redirect_to requests_path
     end
 
     private
 
     def friendship_params
-        params.permit(:user_id, :friend_id)
+        params.permit(:status)
     end
 
 end
