8417aad49412635e3de5692efb51f844554e33e3
Nurislam Ab <you@example.com>
2020-07-22 18:07:20 +0500

    Change timeline posts to show only friends and current user posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -20,9 +20,9 @@ class PostsController < ApplicationController
   private
 
   def timeline_posts
-    @timeline_posts ||= Post.all.ordered_by_most_recent.includes(:user)
+    @timeline_posts ||= Post.where("user_id IN (?) OR user_id = ? ", current_user.friends, current_user.id).order('created_at DESC')
   end
-
+  
   def post_params
     params.require(:post).permit(:content)
   end
