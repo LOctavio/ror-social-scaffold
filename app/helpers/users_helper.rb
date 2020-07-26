2c01c287ae6520dc193c48aff980cd0558e7763f
Nurislam Ab <you@example.com>
2020-07-26 14:28:08 +0500

    Refactor suer helper methods for the views

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -5,7 +5,7 @@ module UsersHelper
             friend_request = current_user.friend_requests.find { |friend| friend.id == user.id }
 
             if friend_request
-                friendship_confirmation_buttons
+                link_to('Friend request', requests_path, class: "friend-request-btn")
             elsif pending_friend
                 button_to('Pending', user_path(user), disabled: true)
             else
@@ -17,12 +17,26 @@ module UsersHelper
     def users_list
         user_list_html = ''
         @users.each do |user|
-
             user_list_html += <<-HTML
-            <li>
-                #{link_to user.name,  user_path(user), class: 'profile-link'}
-                #{friend_request_button_handler(user)}
-            </li>
+            <div class="user-preview-card">
+                <div class="avatar avatar-lg">
+                    <div class="avatar-image-wrapper">
+                        #{image_tag(user.profile_image_path, class: "avatar-image", alt: user.name)}
+                    </div>
+
+                    <div class="avatar-content">
+                        <div class="avatar-name">
+                            <strong>#{link_to(user.name, user_path(user))}</strong>
+                        </div>
+                        <div class="avatar-friend-count">
+                            #{pluralize(user.friends.count, 'friend')}
+                        </div>
+                    </div>
+                </div>
+                <div class="friendship-buttons">
+                    #{friend_request_button_handler(user)}
+                </div>
+            </div>
             HTML
         end
         user_list_html.html_safe
@@ -33,8 +47,26 @@ module UsersHelper
 
         @friend_requests.each do |fr|
             friendship_html += <<-HTML
-            #{ button_to "Confirm", update_friendship_path({friendship: fr, status: true}), method: 'patch'}
-            #{ button_to "Delete request", delete_request_path(friendship: fr), method: 'delete'}
+            <div class="user-preview-card">
+                <div class="avatar avatar-lg">
+                    <div class="avatar-image-wrapper">
+                        #{image_tag(fr.user.profile_image_path, class: "avatar-image", alt: fr.name)}
+                    </div>
+
+                    <div class="avatar-content">
+                        <div class="avatar-name">
+                            #{link_to(fr.name, fr.user)}
+                        </div>
+                        <div class="avatar-friend-count">
+                            #{pluralize(fr.user.friends.count, 'friend')}
+                        </div>
+                    </div>
+                </div>
+                <div class="friendship-buttons">
+                    #{ button_to "Confirm", update_friendship_path({friendship: fr, status: true}), method: 'patch'}
+                    #{ button_to "Delete request", delete_request_path(friendship: fr), method: 'delete', class: 'delete-fr-btn'}
+                </div>
+            </div>            
             HTML
         end
 
